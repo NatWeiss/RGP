8dcd868ee56276c47e7016e04caaeb6c2d209621
Nat Weiss <nat@natweiss.com>
2014-04-14 01:12:55 -0700

    Deleting bin and gen folders.
    Former-commit-id: ad306e8d449c1963a0f1ffbdbf396047056b0cf6

--- src/facebook/proj.android/facebook-android-sdk/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/nat/code/RapidGamePro/src/facebook/proj.android/facebook-android-sdk/bin/AndroidManifest.xml : \
-/Users/nat/code/RapidGamePro/src/facebook/proj.android/facebook-android-sdk/AndroidManifest.xml \
