6e053c68dbd11fe901775a90625fcadd6e7c5408
Nat Weiss <nat@natweiss.com>
2014-05-13 23:51:18 -0700

    Moved include/, java/ and jsb/ to cocos2d/x/.
    Former-commit-id: ad828acd0fbd88af66240cc5f5c8472e288246d9

---
+++ cocos2d/x/java/mk/cocos2d-x/plugin/protocols/proj.android/jni/Android.mk
@@ -0,0 +1,41 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := PluginProtocolStatic
+
+LOCAL_MODULE_FILENAME := libPluginProtocolStatic
+
+LOCAL_SRC_FILES :=\
+$(addprefix ../../platform/android/, \
+	PluginFactory.cpp \
+    PluginUtils.cpp \
+    PluginProtocol.cpp \
+    ProtocolAnalytics.cpp \
+    ProtocolIAP.cpp \
+    ProtocolAds.cpp \
+    ProtocolShare.cpp \
+    ProtocolUser.cpp \
+    ProtocolSocial.cpp \
+) \
+../../PluginManager.cpp \
+../../PluginParam.cpp
+
+LOCAL_CFLAGS := -Wno-psabi
+LOCAL_EXPORT_CFLAGS := -Wno-psabi
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../include $(LOCAL_PATH)/../../platform/android \
+ 	$(LOCAL_PATH)/../../../../cocos \
+ 	$(LOCAL_PATH)/../../../../cocos/2d \
+ 	$(LOCAL_PATH)/../../../../cocos/base \
+ 	$(LOCAL_PATH)/../../../../cocos/2d/platform/android/jni \
+ 	$(LOCAL_PATH)/../../../../cocos/2d/platform/android
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include $(LOCAL_PATH)/../../platform/android
+
+LOCAL_LDLIBS := -landroid
+LOCAL_LDLIBS += -llog
+LOCAL_STATIC_LIBRARIES := android_native_app_glue
+
+include $(BUILD_STATIC_LIBRARY)
+
+$(call import-module,android/native_app_glue)
