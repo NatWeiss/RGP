93f9fd6265328054a9b593eddd4454c6ffff40d6
Nat Weiss <nat@natweiss.com>
2014-06-04 15:58:49 -0700

    Moved prebuild headers and other files to version folder.
    Former-commit-id: b4b22172d8fcbb3a3199c200bee73d440eb7f8ef

--- cocos2d/x/java/android-store/src/com/soomla/store/exceptions/NotEnoughGoodsException.java
+++
@@ -1,7 +0,0 @@
-package com.soomla.store.exceptions;
-
-public class NotEnoughGoodsException extends Exception{
-    public NotEnoughGoodsException(String itemId) {
-        super("You tried to equip virtual good with itemId: " + itemId + " but you don't have any of it.");
-    }
-}
